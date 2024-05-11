6c7f14f9998f50fac3b88d505e401955df42db59
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2024-05-11 11:06:43 +0530

    fix next-config issue.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 import "../globals.css";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Auth",
@@ -18,9 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang='en'>
-        <body className= "bg-dark-1">{children}</body>
+        <body className="bg-dark-1">{children}</body>
       </html>
     </ClerkProvider>
   );
